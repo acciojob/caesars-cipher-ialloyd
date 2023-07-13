eae76a5d78bb5c59273ced2e8e60cd47fe0d6221
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-13 15:33:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,29 +34,13 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-	for(let x of encodedStr){
 
-		if(x.match(/[A-M]/)){
-
-			decodedArr.push(String.fromCharCode(x.charCodeAt(0)+13))
-		}
-		else if(x.match(/[N-Z]/)){
-
-			decodedArr.push(String.fromCharCode((x.charCodeAt(0)+13)-26))
-		}
-		else{
-
-			decodedArr.push(x)
-			
-		}
-	}
-
-  return decodedArr; //return decodedArr
+  return; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-//alert(rot13("SERR YBIR? NPPVBWBO"));
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
